27581bdbb04c4443d3e96c002e7af841a35facb5
Cristian Galiano <c.w.galiano@hotmail.com>
2018-04-19 11:12:03 -0300

    Finalizado

---
+++ Ejercicio 2/consulta.sql
@@ -0,0 +1,8 @@
+select * from personas;
+select * from cursos;
+select * from contactos;
+select * from alumnos;
+select * from docentes;
+select * from es_alumno_de;
+select * from es_docente_de;
+
