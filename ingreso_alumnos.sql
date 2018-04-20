fba3fa73e4be580ec7cf7199bcbbc72b99ca0457
cwgaliano <w.c.galiano@gmail.com>
2018-04-20 14:15:34 -0300

    eliminacion

--- ingreso_alumnos.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO  personas(dni, nombre, apellido, fecha_nac) VALUES(39655715, 'jorge', 'villanueva', '1993-12-10');
-
-INSERT INTO  contactos(dni, direccion, telefono, email) VALUES(39655715, 'villa fiorito', 15566684, '');
-
-INSERT INTO  alumnos(dni, fecha_inscripcion) VALUES(39655715, '2012-12-12');
-
-INSERT INTO  cursos(nombre, descripcion, costo) VALUES('Sistemas operativos', 'linux', 123.9);
-
-INSERT INTO  es_alumno_de(nro_legajo, codigo) VALUES(1, 1);
